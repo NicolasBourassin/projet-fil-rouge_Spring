6f5f5809f07c2964685364747c95693d13be9241
NicolasBourassin <bourassinnicolas@gmail.com>
2023-09-10 14:57:44 +0200

    struggling with 'Could not determine recommended JdbcType' error.

--- projet-fil-rouge_Spring-main/src/test/java/com/example/projetfilrouge_G2/TicketRepositoryTests.java
+++ projet-fil-rouge_Spring-main/src/test/java/com/example/projetfilrouge_G2/TicketRepositoryTests.java
@@ -14,31 +14,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-public class *TicketRepositoryTests {
+public class TicketRepositoryTests {
     @Autowired
     private TicketRepository ticketRepository;
 
     @Test
     public void testFindTicketsByEventContainingAndDateBefore() {
-        // Create a few Ticket entities for testing
-
+        //Arrange
 
         Ticket ticket1 = new Ticket(LocalDate.now().minusDays(1),"Event 1", 10.00F);
         Ticket ticket2 = new Ticket(LocalDate.now().plusDays(1), "Event 2", 10.00F);
         Ticket ticket3 = new Ticket(LocalDate.now().minusDays(2), "Event 3",10.00F);
         Ticket ticket4 = new Ticket(LocalDate.now().minusDays(2), "UNMATCHING", 10.00F);
 
-
-        // Save the tickets to the repository
         ticketRepository.save(ticket1);
         ticketRepository.save(ticket2);
         ticketRepository.save(ticket3);
         ticketRepository.save(ticket4);
-        // Perform the search
+        // Assess
         List<Ticket> tickets = ticketRepository.findTicketsByEventContainingAndDateBefore("Event", LocalDate.now());
 
         // Assert
-        // Check if the search results contain the expected tickets
+        // Check if search results contain the expected tickets
         assertEquals(2, tickets.size());
         assertTrue(tickets.contains(ticket1));
         assertTrue(tickets.contains(ticket3));

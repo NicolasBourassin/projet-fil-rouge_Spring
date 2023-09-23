34f4adf2e29121add573797baffa2ca4cb633b9f
NicolasBourassin <bourassinnicolas@gmail.com>
2023-09-23 16:01:17 +0200

    Transaction creation OK, but only last Transaction is kept in the SellingHistory of the User. Should keep all Transactions.

--- src/main/java/com/example/projetfilrouge_Spring/repository/entity/User.java
+++ src/main/java/com/example/projetfilrouge_Spring/repository/entity/User.java
@@ -144,8 +144,8 @@ public class User implements UserDetails {
                 ", photoUrl='" + photoUrl + '\'' +
                 ", email='" + email + '\'' +
                 ", roleList=" + roleList +
-                ", purchaseHistory=" + purchaseHistory +
-                ", sellingHistory=" + sellingHistory +
+                ", purchaseHistory size=" + purchaseHistory.size() +
+                ", sellingHistory size=" + sellingHistory.size() +
                 '}';
     }
 }
